65d96d9fe36a90e72edaa914464597bf4b7ad746
Ismail <ishmemon@LAPTOP-77J4KGQL>
2024-09-28 11:00:36 -0700

    push the python version as well

---
+++ src_python/url_recipe_fetcher.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+
+# Suppress only the specific SSL warning from urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class URLFetcher:
+    def scrape_recipe(url):
+        response = requests.get(url, verify=False)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        recipe = {}
+        
+        # Get the recipe name from various possible tags
+        title = soup.find(['h1', 'h2', 'h3'])
+        recipe['name'] = title.text if title else 'No title found'
+        
+        # Try to find ingredients from various tags
+        ingredients = soup.find_all(['li', 'p'], class_=lambda x: x and 'ingredient' in x.lower())
+        if not ingredients:
+            ingredients = soup.find_all(['li', 'p'], text=lambda x: x and 'ingredient' in x.lower())
+        recipe['ingredients'] = [ingredient.get_text(strip=True) for ingredient in ingredients]
+        
+        # Try to find instructions from various tags
+        instructions = soup.find_all(['li', 'p'], class_=lambda x: x and 'instruction' in x.lower())
+        if not instructions:
+            instructions = soup.find_all(['li', 'p'], text=lambda x: x and 'instruction' in x.lower())
+        recipe['instructions'] = [instruction.get_text(strip=True) for instruction in instructions]
+        
+        return recipe
---
+++ test_python/test_url_recipe_fetcher.py
@@ -0,0 +1,9 @@
+from src_python.url_recipe_fetcher import URLFetcher
+
+class TestUrlRecipeFetcher:
+
+    @staticmethod
+    def test_single_reipe()
+    url = 'https://ashishbhatia3.wordpress.com/2015/01/21/dhabe-ka-gosht-highway-lamb-curry/amp/'
+    recipe = scrape_recipe(url)
+    assert not (len(recipe) == 0)
