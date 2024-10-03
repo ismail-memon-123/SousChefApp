e89dae081132e7796f6de467d9cafe7f60966e74
ismail-memon-123 <ismail.memon@vanderbilt.edu>
2024-10-02 22:18:06 -0700

    Used library to make the python successfully parse

--- src_python/url_recipe_fetcher.py
+++ src_python/url_recipe_fetcher.py
@@ -1,37 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
-import urllib3
-
-# Suppress only the specific SSL warning from urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-class URLFetcher:
-    @staticmethod
-    def scrape_recipe(url):
-        response = requests.get(url, verify=False)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        recipe = {}
-        
-        # Get the recipe name from various possible tags
-        title = soup.find(['h1', 'h2', 'h3'])
-        recipe['name'] = title.text if title else 'No title found'
-        
-        # Try to find ingredients from various tags
-        ingredients = soup.find_all(['li', 'p'], class_=lambda x: x and 'ingredient' in x.lower())
-        if not ingredients:
-            ingredients = soup.find_all(['li', 'p'], text=lambda x: x and 'ingredient' in x.lower())
-        recipe['ingredients'] = [ingredient.get_text(strip=True) for ingredient in ingredients]
-        
-        # Try to find instructions from various tags
-        instructions = soup.find_all(['li', 'p'], class_=lambda x: x and 'instruction' in x.lower())
-        if not instructions:
-            instructions = soup.find_all(['li', 'p'], text=lambda x: x and 'instruction' in x.lower())
-        recipe['instructions'] = [instruction.get_text(strip=True) for instruction in instructions]
-        
-        return recipe
+from recipe_scrapers import scrape_html
 
 
-url = 'https://ashishbhatia3.wordpress.com/2015/01/21/dhabe-ka-gosht-highway-lamb-curry/amp/'
-recipe = URLFetcher.scrape_recipe(url)
-print(recipe["instructions"])
+url = "https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/"
+html = requests.get(url).content
+scraper = scrape_html(html, org_url=url)
+print(scraper.ingredients())
+
+
